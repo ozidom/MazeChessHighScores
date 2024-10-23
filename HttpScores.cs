c9bb39064dfb47fd025761665f6f9ea9e680029d
ozidom <dommillar@gmail.com>
2024-10-23 12:22:22 +1100

    This commit includes a successful test where the code is actually successfully quering the cosmosdb and returning data. It's all pretty hacky but it works

--- HttpScores.cs
+++ HttpScores.cs
@@ -1,32 +1,65 @@
-using System.Net;
+using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Microsoft.Azure.Functions.Worker;
+using System.Net;
 
 namespace Mazechess.Function
 {
     public class HttpScores
     {
         private readonly ILogger<HttpScores> _logger;
+        private static readonly string cosmosDbEndpoint = System.Environment.GetEnvironmentVariable("COSMOS_DB_ENDPOINT");
+        private static readonly string cosmosDbKey = System.Environment.GetEnvironmentVariable("COSMOS_DB_KEY");
+        private readonly string databaseId = "MazechessScoreDB";       
+        private readonly string containerId = "scores";
+
+        private CosmosClient cosmosClient;
 
         public HttpScores(ILogger<HttpScores> logger)
         {
             _logger = logger;
+            cosmosClient = new CosmosClient(cosmosDbEndpoint, cosmosDbKey);
         }
 
         [Function("HttpScores")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        public async Task<IActionResult> Run([Microsoft.Azure.Functions.Worker.HttpTrigger(Microsoft.Azure.Functions.Worker.AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new OkObjectResult("Welcome to Azure Functions!");
-        }
+          
+            try
+            {
+                var container = cosmosClient.GetContainer(databaseId, containerId);
 
-        [Function("Ping")]
-        public HttpResponseMessage Ping([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
-        {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
-            return new HttpResponseMessage(HttpStatusCode.OK);
+                string queryString = "SELECT * FROM c ORDER BY c.moves ASC";
+                QueryDefinition query = new QueryDefinition(queryString);
+                FeedIterator<dynamic> resultSet = container.GetItemQueryIterator<dynamic>(query);
+
+                List<dynamic> highScores = new List<dynamic>();
+
+                while (resultSet.HasMoreResults)
+                {
+                    FeedResponse<dynamic> response = await resultSet.ReadNextAsync(); // Await here requires async method
+                    foreach (var item in response)
+                    {
+                        highScores.Add(item);
+                    }
+                }
+
+                return new OkObjectResult(JsonConvert.SerializeObject(highScores));
+            }
+            catch (CosmosException ex)
+            {
+                _logger.LogError($"CosmosDB query failed with error: {ex.Message}");
+                return new StatusCodeResult(500);
+            }
         }
     }
 }
